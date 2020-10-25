a0a8c33166d7bacf3f758074daa510295ecb88fa
SCRedstone <45909156+SCRedstone@users.noreply.github.com>
2020-10-25 01:49:43 -0400

    autonselection fix maybe

--- src/BruhLibrary/userinterface.hpp
+++ src/BruhLibrary/userinterface.hpp
@@ -1,57 +1,84 @@
 #include "main.h"
 #pragma once
-/*it's throwing code errors but thats cuz we have a screwed up header structure where
-if we include this into the main stream with global.hpp, we would have a recursive loop
-of hpp files
-it works in the actual code b/c we include it after global.hpp in main.cpp those*/
-//if u wanna temparaly remove those errors so you can check for actual logic issues,
-//uncomment the line below:
-//#include "global.hpp"
-//this is ripped straght from the old code. Ik it works but its still super sketchy
-//also we may want to work on the delays
-/*void autonselection() {
+using namespace pros;
+  /*it's throwing code errors but thats cuz we have a screwed up header structure where
+  if we include this into the main stream with global.hpp, we would have a recursive loop
+  of hpp files
+  it works in the actual code b/c we include it after global.hpp in main.cpp those*/
+  //if u wanna temparaly remove those errors so you can check for actual logic issues,
+  //uncomment the line below:
+#include "globalvar.hpp"
+  //this is ripped straght from the old code. Ik it works but its still super sketchy
+  //also we may want to work on the delays
+void autonselection(){
+  int msDelay = 50; // Delay in ms
   ctrl.clear();
-  while (true) {
-    delay(50);
-    if (!confirmedauton) {
-      if (ctrl.get_digital_new_press(DIGITAL_LEFT)) {
+
+  // Main Loop
+  while(true){
+    delay(msDelay);
+
+    // When unconfirmed
+    while(!confirmedauton){
+      if(ctrl.get_digital_new_press(DIGITAL_LEFT)){
         selectedauton--;
-      } else if (ctrl.get_digital_new_press(DIGITAL_RIGHT)) {
+        if(selectedauton < 0){
+          selectedauton = 4;
+        }
+      }
+      else if(ctrl.get_digital_new_press(DIGITAL_RIGHT)){
         selectedauton++;
-      } else if (ctrl.get_digital_new_press(DIGITAL_A))
+        if(selectedauton > 4){
+          selectedauton = 0;
+        }
+      }
+
+      // A to confirm selection
+      else if(ctrl.get_digital_new_press(DIGITAL_A)){
         confirmedauton = true;
-      if (selectedauton >= 5)
-        selectedauton = 0;
-      if (selectedauton < 0)
-        selectedauton = 4;
-      //    if (selectedauton != lastselectedauton){
-      //  delay(50);
-      switch (selectedauton) {}
-      delay(50);
-      //  }
+      }
+      /*if (selectedauton != lastselectedauton){
+      delay(msDelay);
+      switch(selectedauton){} ?? */
+      delay(msDelay);
+      //}
     }
-    if (confirmedauton) {
-      ctrl.clear();
-      delay(50);
-      ctrl.set_text(0, 0, "AUTON SELECTED");
-      delay(50);
-      ctrl.set_text(1, 0, "PRESS B TO confirm");
-      delay(50);
-      ctrl.set_text(2, 0, "PRESS A TO CANCEL");
-      if (ctrl.get_digital_new_press(DIGITAL_A)) {
+
+    ctrl.clear();
+    delay(msDelay);
+    ctrl.set_text(0, 0, "AUTON SELECTED");
+    delay(msDelay);
+    ctrl.set_text(1, 0, "PRESS B TO confirm");
+    delay(msDelay);
+    ctrl.set_text(2, 0, "PRESS A TO CANCEL");
+
+    bool bCheck = false; // for double break
+
+    // When confirmed
+    while(confirmedauton){
+
+      // If auton select cancelled
+      if(ctrl.get_digital_new_press(DIGITAL_A)){
         ctrl.clear();
-        delay(50);
+        delay(msDelay);
         ctrl.set_text(0, 0, "SELECT AUTON");
-        delay(50);
+        delay(msDelay);
         ctrl.set_text(2, 0, "PRESS A TO CONFIRM");
-        delay(50);
+        delay(msDelay);
         confirmedauton = false;
       }
-      if (ctrl.get_digital_new_press(DIGITAL_B))
+
+      // If confirmed
+      else if (ctrl.get_digital_new_press(DIGITAL_B)){
+        bCheck = true;
         break;
+      }
     }
-    delay(50);
+    if(bCheck){
+      break;
+    }
+    delay(msDelay);
   }
   ctrl.clear();
-  ctrl.set_text(0, 0, "PROGRAM START"); //
-}*/
+  ctrl.set_text(0, 0, "PROGRAM START");
+}
